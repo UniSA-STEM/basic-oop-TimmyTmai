cdd7610b2d878673f5cf2ebe424d0838a824a597
TimmyTmai <160735110+TimmyTmai@users.noreply.github.com>
2025-10-14 20:31:39 +1100

    Define all classes Attributes

--- Hacker.py
+++ Hacker.py
@@ -6,3 +6,11 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Hacker:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__inventory = []
+        self.__rig = None
+        self.__trace_level: int = 0
+        self.__trace_threshold: int = 5
+
--- Rig.py
+++ Rig.py
@@ -6,3 +6,10 @@ ID: <student_id>
 Username: <username>
 This is my own work as defined by the University's Academic Misconduct Policy.
 """
+class Rig:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__damage = 0
+        self.__broken = False
+        self.__storage = []
+        self.__upgrade_level = 0
